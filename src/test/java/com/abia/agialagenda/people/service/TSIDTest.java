7031e24b49894f38c865a5d97656750f32814290
Jeferson Silva <jefersoncj@outlook.com>
2025-12-07 00:20:50 -0300

    Criando gerador de ids

---
+++ src/test/java/com/abia/agialagenda/people/service/TSIDTest.java
@@ -0,0 +1,21 @@
+package com.abia.agialagenda.people.service;
+
+
+import com.abia.agialagenda.people.service.commom.IdGenerator;
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+class TSIDTest {
+
+    @Test
+    void shouldGenerateTSID() {
+        TSID tsid = IdGenerator.generateTSID();
+        Assertions.assertThat(tsid.getInstant())
+                .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+    }
+
+}
